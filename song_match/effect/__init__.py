b2b017a5d670da50ccaf205149cf8f8946016436
G Roques <groques360@gmail.com>
2018-04-04 19:52:27 -0500

    Add more information to docstring

--- song_match/effect/__init__.py
+++ song_match/effect/__init__.py
@@ -1,6 +1,7 @@
 """
-  * :class:`~song_match.effect.effect.Effect`
-  * :class:`~song_match.effect.effect.EffectFactory`
+  * :class:`~song_match.effect.effect.Effect` - Abstract base class for game effects.
+  * :class:`~song_match.effect.effect.factory.EffectFactory` - Factory for creating
+    `~song_match.effect.effect.Effect` instances.
 """
 
 from .effect import Effect
